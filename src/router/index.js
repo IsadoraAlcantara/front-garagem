2f7b1a3ca401e41e9bb23bbdb979dc0974bf425f
Isadora Alcantara <106554588+IsadoraAlcantara@users.noreply.github.com>
2024-07-23 14:01:36 -0300

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LayoutFull from '@/layouts/LayoutFull.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: '',
+      component: LayoutFull,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: HomeView
+        },
+        {
+          path: '/login',
+          name: 'Login',
+          component: () => import('../views/LoginView.vue')
+        },
+        {
+          path: '/produtos/adicionar',
+          name: 'ProductAdd',
+          component: () => import('../views/ProductAdd.vue')
+        }
+      ]
+    }
+  ]
+})
+
+export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import '@passageidentity/passage-elements/passage-auth'
+</script>
+<template>
+  <h1>Página de Login</h1>
+  <div class="authContainer">
+    <passage-auth app-id="NHCUTXYQSJjApyaPHan94R1P"></passage-auth>
+  </div>
+</template>
