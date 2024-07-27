e8ae596415f92ded52e02828ea917e23bc797ab2
IsadoraAlcantara <Isadoraalca405@gmail.com>
2024-07-26 22:23:33 -0300

    ajuste no título da home

--- src/components/templates/large/LargeMain.vue
+++ src/components/templates/large/LargeMain.vue
@@ -1,8 +1,11 @@
 <script setup></script>
 <template>
-    <div>
-        Main Large
-    </div>
+    <h1>
+        Home
+    </h1>
 </template>
 <style scoped>
+h1 {
+    text-align: center;
+}
 </style>
